300f29807f4f2fcaa29bafda44a0a67765237216
styller1 <pcraveirodeandrade@gmail.com>
2022-05-03 11:05:18 -0300

    Novas anotações

---
+++ Html/Site/Index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+	<title> Meu primeiro site em PHP!</title>
+</head>
+
+<body>
+	
+	<?php
+		for ( $i= 0; $i <11 ; $i++) {
+		print ( "Linha numero". $i . "<br />");
+		}
+		?>
+		</body>
+</html>		
